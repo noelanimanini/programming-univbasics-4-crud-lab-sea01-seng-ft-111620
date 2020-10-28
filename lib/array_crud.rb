250e21963eff5b1e0d570cdbcbd505d16b1af051
crystal  villanueva <noelani.villanueva@gmail.com>
2020-10-28 05:43:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ create_an_array = ["Zeus", "Hades", "Poseidon", "Hera"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"]
+  element = "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
