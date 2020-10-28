196d52f532205970fe0ffc72a14e6118aa8ae963
crystal  villanueva <noelani.villanueva@gmail.com>
2020-10-28 06:02:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,6 +33,7 @@ end
 
 def retrieve_first_element_from_array(array)
 retrieve_first_element_from_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+retrieve_first_element_from_array[-1]
 end
 
 def retrieve_last_element_from_array(array)
