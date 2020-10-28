aa77a8737eb40d6ea82ff8e457a15b041e685847
crystal  villanueva <noelani.villanueva@gmail.com>
2020-10-28 05:50:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   add_element_to_start_of_array = ["I", "am", "really", "learning"]
-  add_element_to_start_of_array = "wow".unshift
+  add_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
