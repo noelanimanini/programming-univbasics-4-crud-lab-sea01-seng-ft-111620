0c19e59e2498a72925f57334213650741faf6640
crystal  villanueva <noelani.villanueva@gmail.com>
2020-10-28 06:01:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,11 +28,11 @@ end
 
 def retrieve_element_from_index(array, index_number)
 retrieve_element_from_index = ["wow", "I", "am", "really", "learning", "arrays!"]
-retrieve_element_from_index[2] 
+retrieve_element_from_index[2]
 end
 
 def retrieve_first_element_from_array(array)
-
+retrieve_first_element_from_array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def retrieve_last_element_from_array(array)
